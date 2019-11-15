8daf04a1058e374e685e7e6481bee85b4c2bf39a
zeac084 <zeac084@live.rhul.ac.uk>
2019-11-15 18:52:09 +0000

    Added build tools confs for pom file

--- src/main/java/com/rhul/springboot/ConcurrentCheckersApplication.java
+++ src/main/java/com/rhul/springboot/ConcurrentCheckersApplication.java
@@ -3,6 +3,7 @@ package com.rhul.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 
 //Main runnable class.
 @RestController
+@Controller
 @EnableWebSocket
 @SpringBootApplication
 public class ConcurrentCheckersApplication implements WebSocketConfigurer {
